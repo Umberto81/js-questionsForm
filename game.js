99a411f4432b10943de9862054bc5418d9d29fcf
Umberto81 <goffredo.gonnella@gail.com>
2022-05-11 12:29:26 +0100

    added getNewQuestion funcs

--- game.js
+++ game.js
@@ -33,3 +33,27 @@ let questions = [
     answer: 4,
   },
 ];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startgame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+};
+
+startgame();
